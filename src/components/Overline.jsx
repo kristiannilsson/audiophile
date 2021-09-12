5be672ff87492c5d418e2b4b8253531257f58018
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-09-13 01:41:53 +0200

    Create Overline.jsx

---
+++ src/components/Overline.jsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Paragraph = styled.p`
+  font-size: 14px;
+  letter-spacing: 10px;
+  color: var(--sienna);
+`;
+
+export default function Overline() {
+  return <Paragraph>NEW PRODUCT</Paragraph>;
+}
