e9561c2f36e5de74fac0fd96b8e4edcff7ab8c9b
kristiannilsson <77685746+kristiannilsson@users.noreply.github.com>
2021-09-18 22:35:01 +0200

    updated CSP policy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+const { createSecureHeaders } = require("next-secure-headers");
+
 module.exports = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  async headers() {
+    return [{ source: "/(.*)", headers: createSecureHeaders() }];
+  },
   reactStrictMode: true,
-}
+};
